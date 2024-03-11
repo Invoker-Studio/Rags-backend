6905760de5d04e15cf118d9cf453bbc46a952cea
mojtaba-1180 <mohammadsgrdev@gmail.com>
2024-03-11 10:59:38 +0330

    added deploy shell

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Install dependencies
+npm install
+
+# Build the projects
+nx build rider-api
+nx build driver-api
+nx build admin-api
+# Add more build commands as necessary
+
+# delete pm2 builds
+pm2 delete rider-api driver-api admin-api
+# Deploy with PM2
+# Make sure the dist paths match where Nx outputs the builds
+pm2 start dist/apps/rider-api/main.js --name rider-api
+pm2 start dist/apps/driver-api/main.js --name driver-api
+pm2 start dist/apps/admin-api/main.js --name admin-api
+# Repeat for other services as necessary
+
+# Save PM2 list
+pm2 save
+
+# Restart PM2 on system reboot
+pm2 startup
