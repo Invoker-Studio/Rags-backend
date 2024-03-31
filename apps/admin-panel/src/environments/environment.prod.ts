a4ba5eda0bc619c042787fa62c648fd71b5f1e55
mojtaba-1180 <mohammadsgrdev@gmail.com>
2024-03-31 13:30:18 +0330

    update env

--- apps/admin-panel/src/environments/environment.prod.ts
+++ apps/admin-panel/src/environments/environment.prod.ts
@@ -1,10 +1,5 @@
 export const environment = {
   production: true,
-  root: `${
-    window.location.protocol
-  }//api.${window.location.hostname.toString()}`,
-  wsEndpoint: `${window.location.protocol.replace(
-    "http",
-    "ws",
-  )}//api.${window.location.hostname.toString()}/graphql`,
+  root: `https://api.ragstaxi.com`,
+  wsEndpoint: `ws://api.ragstaxi.com/graphql`,
 };
